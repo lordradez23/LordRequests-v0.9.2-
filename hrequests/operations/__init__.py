8064f116d0ba17135e565e6479bde0a025d11f2d
Lordradeez <lordradez362@gmail.com>
2026-05-30 18:13:50 +0100

    Step 1: Initialize Operations & Automation foundation

---
+++ hrequests/operations/__init__.py
@@ -0,0 +1,10 @@
+'''
+LordRequests Operations & Automation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Advanced tools for operational resilience, behavioral stealth, 
+and automated challenge bypass.
+'''
+
+from .jitter import Jitter
+# Future imports will be added here
