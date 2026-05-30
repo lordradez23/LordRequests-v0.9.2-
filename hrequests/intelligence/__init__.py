5ebc11887e2cc1dfd199b3fc6c51ddae4ec3bab4
Lordradeez <lordradez362@gmail.com>
2026-05-30 18:02:31 +0100

    Step 1: Initialize intelligence foundation module

---
+++ hrequests/intelligence/__init__.py
@@ -0,0 +1,10 @@
+'''
+LordRequests Intelligence Suite
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A suite of advanced tools for stealth networking, anti-censorship,
+and digital intelligence gathering.
+'''
+
+from .shadow_api import to_api_script
+# Future imports will be added here
