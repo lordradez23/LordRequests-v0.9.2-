80eebbe4b71190a7c967f05b1dfa3209af5f56cd
Lordradeez <lordradez362@gmail.com>
2026-05-31 01:33:15 +0100

    feat: Implement Intelligent Jitter Heuristics (Feature 87)

--- hrequests/operations/jitter.py
+++ hrequests/operations/jitter.py
@@ -27,15 +27,51 @@ class Jitter:
             return
         
         delay = random.uniform(self.min_delay, self.max_delay)
-        # Add a "micro-jitter" to make it even less predictable
-        delay += random.uniform(-0.05, 0.05)
-        
         if delay > 0:
             time.sleep(max(0, delay))
 
-    @classmethod
-    def apply(cls, persona: str):
+class IntelligentJitter(Jitter):
+    '''
+    Context-aware jitter that adapts to time-of-day habits.
+    '''
+    HEURISTICS = {
+        'workday': {
+            'peak_times': [9, 10, 11, 13, 14, 15, 16], # 9am-5pm except lunch
+            'slow_times': [12, 17, 18],              # Lunch and end of day
+            'base_multiplier': 1.0,
+            'slow_multiplier': 2.5
+        },
+        'nightowl': {
+            'peak_times': [22, 23, 0, 1, 2],         # Late night activity
+            'slow_times': [10, 11, 12, 13, 14, 15],  # Morning/Afternoon sleep
+            'base_multiplier': 0.8,                  # Faster at night
+            'slow_multiplier': 3.0
+        }
+    }
+
+    def __init__(self, persona: str = 'researcher', heuristic: str = 'workday'):
+        super().__init__(persona)
+        self.heuristic = self.HEURISTICS.get(heuristic, self.HEURISTICS['workday'])
+
+    def get_context_delay(self) -> float:
         '''
-        Helper to wait immediately for a persona.
+        Calculates delay with heuristic modifiers.
         '''
-        cls(persona).wait()
+        import datetime
+        current_hour = datetime.datetime.now().hour
+        
+        delay = random.uniform(self.min_delay, self.max_delay)
+        
+        if current_hour in self.heuristic['peak_times']:
+            delay *= self.heuristic['base_multiplier']
+        elif current_hour in self.heuristic['slow_times']:
+            delay *= self.heuristic['slow_multiplier']
+        else:
+            delay *= 1.5 # Neutral time
+            
+        return max(0, delay)
+
+    def wait(self):
+        delay = self.get_context_delay()
+        if delay > 0:
+            time.sleep(delay)
